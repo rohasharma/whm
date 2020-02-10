d1a9c20335e168d97fc13db916eed64cdc6248e1
rohasharma <42043940+rohasharma@users.noreply.github.com>
2020-02-10 18:19:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ Set configurations required for the application.
 
 import os
 
-MYSQL_DATABASE_PASSWORD = "e4fc6a65f379296c804f"
-DB_SERVER_IP = "localhost"
-MYSQL_DATABASE_USER = 'root'
+MYSQL_DATABASE_PASSWORD = "<PASSWORD>"
+DB_SERVER_IP = "<DB SERVER IP>"
+MYSQL_DATABASE_USER = '<DB USERNAME>'
 MYSQL_DATABASE_DB = 'whmdb'
 SQLALCHEMY_DATABASE_URI = 'mysql://root:' + MYSQL_DATABASE_PASSWORD + '@' \
                           + DB_SERVER_IP + '/' + MYSQL_DATABASE_DB
